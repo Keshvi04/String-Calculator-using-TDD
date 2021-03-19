85f7d43c3dfa6e1a13277c1c63e5f9c41fb6737e
Keshvi04 <keshvibhimani@gmail.com>
2021-03-19 20:19:19 +0530

    first commit task1

---
+++ task1_test.java
@@ -0,0 +1,36 @@
+package com.wordpress.technologyconversations.tddtest;
+ 
+import org.junit.Test;
+import com.wordpress.technologyconversations.tdd.StringCalculator;
+import java.io.*;
+
+class GFG {
+	public class StringCalculatorTest {
+        @Test
+        public final void func1() {                     //when an empty string is passed.
+            StringCalculator.add("");
+            Assert.assertTrue(true);                    //sum=0;
+        }
+            @Test
+        public final void func2() {                     //when one string is passed.
+            StringCalculator.add("1");
+            Assert.assertTrue(true);                    //sum=1;
+        }
+            @Test
+        public final void func3() {                     //when two strings are passed.
+            StringCalculator.add("1,2");
+            Assert.assertTrue(true);                    //sum=3;
+        }
+            @Test
+        public final void func4() {                     //when more than two strings are passed.
+            StringCalculator.add("1,2,3");
+            throw new IllegalArgumentException("Exactly 2 parameters required !");
+        }
+            @Test
+        public final void func4() {                     //when non number are passed.
+            StringCalculator.add("1,A");
+            throw new NumberFormatException("Input string should be in numeric types");
+        }
+        
+	}
+}
